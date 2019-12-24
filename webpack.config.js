c29ff6bb82196d989ab9f76a928f549fe3c6790c
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2019-12-24 15:13:44 -0800

    Add source maps

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,11 @@ const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: './source/javascripts/script.js',
   devServer: {
     contentBase: path.join(__dirname, 'public'),
   },
+  devtool: 'source-map',
   plugins: [
     new CopyPlugin([
       { from: 'public' },
